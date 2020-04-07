1baaade56b69b12ad6c21b48ede9425ee798c37b
Heydar Gerayzade <hgarayzada@gmail.com>
2020-04-07 15:39:04 +0400

    sider done responsive

--- components/layout/index.js
+++ components/layout/index.js
@@ -6,8 +6,16 @@ import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 const { Header, Content, Footer, Sider } = Layout;
 
 const PageLayout = ({ children, title }) => {
-  React.useEffect(() => {window.scrollTo(0,0)}, []); 
-  const [collapsed, toggleCollapse] = React.useState(false);
+  // interactions with sidebar
+  const [sider, setSider] = React.useState({broken: false, collapsed: false});
+  const collapseSider = (collapsed) => setSider({...sider, collapsed: collapsed});
+  const breakSider = (broken) => setSider({...sider, broken: broken});
+  // when component first mounted
+  React.useEffect(() => {
+    // scroll document up
+    window.scrollTo(0,0)
+    // update state if broken
+  }, []); 
   return (
     <div className="wrapper">
       <Head>
@@ -20,19 +28,25 @@ const PageLayout = ({ children, title }) => {
         <PreloadFonts />
       </Head>
       <Layout>
-        <Sider className={'page-sidebar' + (collapsed ? ' collapsed' : '')} theme="dark" width="250">
+        <Sider 
+          className={'page-sidebar' + (sider.collapsed ? ' collapsed' : '')} 
+          theme="dark" 
+          width="250"
+          breakpoint="xl"
+          onBreakpoint={(broken) => setSider({broken: broken, collapsed: broken})}
+        >
           <div className="logo">Chipi Chipi</div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
+          <Menu className="page-sidebar-menu" theme="dark" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" className="page-sidebar-menu-item">
               <span className="nav-text">{title}</span>
             </Menu.Item>
           </Menu>
         </Sider>
-        <Layout className="page-layout">
+        <Layout className="page-layout" onClick={() => sider.broken && !sider.collapsed ? collapseSider(true) : null}>
           <Header className="page-header">
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            {React.createElement(sider.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
               className: 'trigger',
-              onClick: () => toggleCollapse(!collapsed),
+              onClick: () => {collapseSider(!sider.collapsed)}
             })}
             <h2>{title}</h2>
           </Header>
